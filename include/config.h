5f8bc5cabf5fbfff7b27c01c717ab66e0056679a
unknown <ogieniewski.dev@gmail.com>
2024-02-17 02:42:48 +0100

    Fixed formatting

--- include/config.h
+++ include/config.h
@@ -4,5 +4,4 @@
 #define MAX_NUMERAL_SYSTEM 16
 #define MAX_TIME_LIMIT_SECONDS 5
 
-static const char system_representation[MAX_NUMERAL_SYSTEM - MIN_NUMERAL_SYSTEM + 2] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-																						'A', 'B', 'C', 'D', 'E', 'F'};
+static const char system_representation[MAX_NUMERAL_SYSTEM - MIN_NUMERAL_SYSTEM + 2] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
